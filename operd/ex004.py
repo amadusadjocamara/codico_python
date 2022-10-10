96c77c11404a3157dc9062c6f835bf0aa4130190
Amadu Sadjo Camara <105577816+amadusadjocamara@users.noreply.github.com>
2022-10-10 08:20:40 -0700

    python

---
+++ operd/ex004.py
@@ -0,0 +1,8 @@
+n = int (input("Digite number: "))
+d = n+n
+tr =  n+n+n
+qr = n**(1/2)
+print("valuer you digite is: {}".format(n))
+print("double: {} ".format(d))
+print("Triple: {}".format(tr))
+print("Squert: {}".format(qr))
