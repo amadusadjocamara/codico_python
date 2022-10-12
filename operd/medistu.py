00fc0670787edb0fdb22f4dca487d91c8962dca9
Amadu Sadjo Camara <105577816+amadusadjocamara@users.noreply.github.com>
2022-10-11 19:34:28 -0700

    meter axercice

---
+++ operd/medistu.py
@@ -0,0 +1,5 @@
+note1 = int(input("Enter frist marks: "))
+note2 = int(input("Enter second marks: "))
+sum = note1 + note2
+media = sum /2
+print("your total marks is {0}.\nyour media is {1} ".format(sum,media))
---
+++ operd/meter.py
@@ -0,0 +1,4 @@
+m = int (input("Enter vsluer for meter: "))
+cm = m * 100
+mm = m * 1000
+print("valuer for meter is {0}, {1} cm and {2} mm.".format(m,cm,mm,))
