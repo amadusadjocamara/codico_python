ca3e3e7aa61c045134a29115aa5b2cacabab1c06
Amadu Sadjo Camara <105577816+amadusadjocamara@users.noreply.github.com>
2023-06-16 19:34:12 +0530

    python
    im o]working in function strptime

---
+++ date/ex001.py
@@ -0,0 +1,15 @@
+import datetime
+
+
+currentday = datetime.date.today()
+userinpt = input ("what is your brithday? ")
+
+birt = datetime.datetime.strptime(userinpt, '%m/%d/%Y')
+
+#why did we list datetime twice
+#because we are calling the strptime function 
+#which is part of the datetime class
+#which is in the datetime module
+#print("Your birth month is " + birthdate.strtime("%B"))
+
+print(birt)
